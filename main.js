250a60c23bd722df69085f5e59b0339fb4373be9
Lauren Giordano <laurengiordano94@gmail.com>
2023-03-21 12:57:55 +1100

    done with lab

--- main.js
+++ main.js
@@ -1,8 +1,49 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+const HEART = 'test';
 
 // Your JavaScript code goes here!
+
+const likeGlyph = document.getElementsByClassName('like-glyph');
+const modal = document.querySelector('#modal');
+
+
+
+
+
+function changeHeart (item) {
+  mimicServerCall()
+  .then(() => {
+    if (item.textContent === EMPTY_HEART) {
+      item.textContent = FULL_HEART;
+      item.classList.add("activated-heart");
+    } else {
+      item.classList.remove("activated-heart");
+      item.textContent = EMPTY_HEART;
+    };
+  })
+  .catch(error => {
+    modal.classList.remove("hidden");
+    document.querySelector('#modal-message').textContent = `${error}`;
+    setTimeout(() => {
+      modal.classList.add("hidden");
+    }, 3000);
+  });
+}
+
+
+
+
+
+
+
+Array.from(likeGlyph).forEach( (item) => {
+  item.addEventListener("click", function () {
+    changeHeart(item);
+  });
+});
+
 
 
 
